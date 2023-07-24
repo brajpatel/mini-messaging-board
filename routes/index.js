68918b2549ee3e4a13deff8557081a8acf2277b6
brajpatel <brajeshpatel0002@gmail.com>
2023-07-24 20:41:04 +0100

    Create sample messages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const messages = [
+    {
+      text: 'Hello!',
+      user: 'Alina',
+      added: new Date()
+    },
+    {
+      text: 'Hey, what\'s up?',
+      user: 'Elise',
+      added: new Date()
+    }
+  ];
+
+  res.render('index', { title: 'Mini Messaging Board', 'messages': messages });
 });
 
 module.exports = router;
