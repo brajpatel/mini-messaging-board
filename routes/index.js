bb224f81d3cb218ac6c53cbfc215ad1432983af4
brajpatel <brajeshpatel0002@gmail.com>
2023-07-26 11:21:41 +0100

    Add name attribute to form inputs

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Hello!',
+    user: 'Alina',
+    added: new Date().toLocaleTimeString() + ' - ' + new Date().toLocaleDateString()
+  },
+  {
+    text: 'Hey, what\'s up?',
+    user: 'Elise',
+    added: new Date().toLocaleTimeString() + ' - ' + new Date().toLocaleDateString()
+  },
+  {
+    text: 'Buh',
+    user: 'Buh',
+    added: new Date().toLocaleTimeString() + ' - ' + new Date().toLocaleDateString()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const messages = [
-    {
-      text: 'Hello!',
-      user: 'Alina',
-      added: new Date().toLocaleTimeString() + ' - ' + new Date().toLocaleDateString()
-    },
-    {
-      text: 'Hey, what\'s up?',
-      user: 'Elise',
-      added: new Date().toLocaleTimeString() + ' - ' + new Date().toLocaleDateString()
-    },
-    {
-      text: 'Buh',
-      user: 'Buh',
-      added: new Date().toLocaleTimeString() + ' - ' + new Date().toLocaleDateString()
-    }
-  ];
-
   res.render('index', { title: 'Mini Messaging Board', messages: messages });
 });
+
+router.post('/new', function(req, res, next) {
+  console.log(req.body)
+})
 
 module.exports = router;
